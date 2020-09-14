07981d028082318d09923f20e92a4a3efb5736a5
Omar Benhmuda <omar2kb@gmail.com>
2020-09-14 14:55:07 -0400

    first commit - added first 2 pokemon hard coded in html to get a feel of how I want the layout to look

---
+++ main.js
@@ -0,0 +1,24 @@
+let pokeNumber = ["001", "002", "003", "004", "005", "006", "007", "008", "009",
+    "010", "011", "012", "013", "014", "015", "016", "017", "018", "019", "020"];
+let pokeNames = ["Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle",
+    "Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot",
+    "Rattata", "Raticate"];
+let pokeType1 = ["Grass", "Grass", "Grass", "Fire", "Fire", "Fire", "Water", "Water", "Water", "Bug", "Bug", "Bug",
+    "Bug", "Bug", "Bug", "Normal", "Normal", "Normal", "Normal", "Normal"];
+let pokeType2 = ["Poison", "Poison", "Poison", null, null, "Flying", null, null, null, null, null, "Flying", "Poison",
+    "Poison", "Poison", "Flying", "Flying", "Flying", null, null];
+let baseHP = [45, 60, 80, 39, 58, 78, 44, 59, 79, 45, 50, 60, 40, 45, 65, 40, 63, 83, 30, 55
+]
+let baseATK = [49, 62, 82, 52, 64, 84, 48, 63, 83, 30, 20, 45, 35, 25, 90, 45, 60, 80, 56, 81
+];
+let baseDEF = [49, 63, 83, 43, 58, 78, 65, 80, 100, 35, 55, 50, 30, 50, 40, 40, 55, 75, 35, 60
+];
+let baseSpATK = [65, 80, 100, 60, 80, 109, 50, 65, 85, 20, 25, 90, 20, 25, 45, 35, 50, 70, 25, 50
+];
+let baseSpDEF = [65, 80, 100, 50, 65, 85, 64, 80, 105, 20, 25, 80, 20, 25, 80, 35, 50, 70, 35, 70
+];
+let baseSPD = [45, 60, 80, 65, 80, 100, 43, 58, 78, 45, 30, 70, 50, 35, 75, 56, 71, 101, 72, 77
+];
+
+
+
